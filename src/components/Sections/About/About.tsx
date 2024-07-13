84280517a92fd3eda2fe8ff7c290fa6d8c0d70d3
Alejandro <alejosotodiaz@hotmail.com>
2024-07-12 20:50:05 -0500

    landing tittle

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -1,32 +1,55 @@
-import Image from "next/image"
-import styles from "./About.module.css"
-import { useTranslation } from "react-i18next"
+import Image from "next/image";
+import styles from "./About.module.css";
+import { useTranslation } from "react-i18next";
 const AboutSection: React.FC = () => {
-    const { t } = useTranslation(["landing"])
-    return (
-        <section id="about" className={styles.aboutUsSection}>
-            <div className={`${styles.bubble} ${styles.bubble1}`}></div>
-            <div className={`${styles.bubble} ${styles.bubble2}`}></div>
-            <div className={`${styles.bubble} ${styles.bubble3}`}></div>
-            <div className={`${styles.bubble} ${styles.bubble4}`}></div>
-            <h2>{t("aboutSection.title")}</h2>
-            <p>{t("aboutSection.description")}</p>
-            <div className={styles.imagesContainer}>
-                <div className={styles.imageBox}>
-                    <Image className={styles.img} src="/background/Security.png" alt="Security" width={280} height={490} />
-                    <span className={styles.imageLabel}>{t("aboutSection.imageDescriptions.security")}</span>
-                </div>
-                <div className={styles.imageBox}>
-                    <Image className={styles.img} src="/background/Speed.png" alt="Speed" width={280} height={490} />
-                    <span className={styles.imageLabel}>{t("aboutSection.imageDescriptions.speed")}</span>
-                </div>
-                <div className={styles.imageBox}>
-                    <Image className={styles.img} src="/background/Efficiency.png" alt="Efficiency" width={280} height={490} />
-                    <span className={styles.imageLabel}>{t("aboutSection.imageDescriptions.efficiency")}</span>
-                </div>
-            </div>
-
-        </section>
-    )
-}
-export default AboutSection
+  const { t } = useTranslation(["landing"]);
+  return (
+    <section id="about" className={styles.aboutUsSection}>
+      <div className={`${styles.bubble} ${styles.bubble1}`}></div>
+      <div className={`${styles.bubble} ${styles.bubble2}`}></div>
+      <div className={`${styles.bubble} ${styles.bubble3}`}></div>
+      <div className={`${styles.bubble} ${styles.bubble4}`}></div>
+      <h2 className={styles.tittle}>{t("aboutSection.title")}</h2>
+      <p>{t("aboutSection.description")}</p>
+      <div className={styles.imagesContainer}>
+        <div className={styles.imageBox}>
+          <Image
+            className={styles.img}
+            src="/background/Security.png"
+            alt="Security"
+            width={280}
+            height={490}
+          />
+          <span className={styles.imageLabel}>
+            {t("aboutSection.imageDescriptions.security")}
+          </span>
+        </div>
+        <div className={styles.imageBox}>
+          <Image
+            className={styles.img}
+            src="/background/Speed.png"
+            alt="Speed"
+            width={280}
+            height={490}
+          />
+          <span className={styles.imageLabel}>
+            {t("aboutSection.imageDescriptions.speed")}
+          </span>
+        </div>
+        <div className={styles.imageBox}>
+          <Image
+            className={styles.img}
+            src="/background/Efficiency.png"
+            alt="Efficiency"
+            width={280}
+            height={490}
+          />
+          <span className={styles.imageLabel}>
+            {t("aboutSection.imageDescriptions.efficiency")}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AboutSection;
