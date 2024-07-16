e3c3e944caa786faec88330b39f89e0b737bb8cd
Alejandro <alejosotodiaz@hotmail.com>
2024-07-15 20:55:24 -0500

    update copy from liquidator

--- src/components/Sections/Services/Services.tsx
+++ src/components/Sections/Services/Services.tsx
@@ -1,26 +1,48 @@
-import Image from "next/image"
-import styles from "./Services.module.css"
-import { useTranslation } from "react-i18next"
+import Image from "next/image";
+import styles from "./Services.module.css";
+import { useTranslation } from "react-i18next";
 const ServicesSection: React.FC = () => {
-    const { t } = useTranslation(["landing"])
-    return (
-        <section id="services" className={styles.servicesSection}>
-            <div className={styles.background}>
-                <div className={styles.circle} id={styles.circle3}></div>
-            </div>
-            <div className={styles.content}>
-                <h2>{t("servicesSection.title")} <span className={styles.gradient}>{t("servicesSection.increase")}</span>.</h2>
-                <p>{t("servicesSection.subtitle")}</p>
-                {/* <button className={styles.viewServicesButton}>{t("servicesSection.contact")}</button> */}
-            </div>
-            <div className={styles.servicesContainer}>
-                <span className={styles.servicesContainerTitle}>{t("servicesSection.price")}</span>
-                <Image className={`${styles.img} ${styles.imgHorizontal}`} src={"/background/Services.png"} alt='' width={860} height={570} />
-                <Image className={`${styles.img} ${styles.imgVertical}`} src={"/background/Services-vertical.png"} alt='' width={800} height={970} />
-                <span className={styles.servicesContainerSubtitle}>{t("servicesSection.find")}</span>
-            </div>
-        </section>
-    )
-}
+  const { t } = useTranslation(["landing"]);
+  return (
+    <section id="services" className={styles.servicesSection}>
+      <div className={styles.background}>
+        <div className={styles.circle} id={styles.circle3}></div>
+      </div>
+      <div className={styles.content}>
+        <h2>
+          {t("servicesSection.title-1")}{" "}
+          <span className={styles.gradient}>
+            {t("servicesSection.increase")}
+          </span>{" "}
+          {t("servicesSection.title-2")}
+        </h2>
+        <p>{t("servicesSection.subtitle")}</p>
+        {/* <button className={styles.viewServicesButton}>{t("servicesSection.contact")}</button> */}
+      </div>
+      <div className={styles.servicesContainer}>
+        <span className={styles.servicesContainerTitle}>
+          {t("servicesSection.price")}
+        </span>
+        <Image
+          className={`${styles.img} ${styles.imgHorizontal}`}
+          src={"/background/Services.png"}
+          alt=""
+          width={860}
+          height={570}
+        />
+        <Image
+          className={`${styles.img} ${styles.imgVertical}`}
+          src={"/background/Services-vertical.png"}
+          alt=""
+          width={800}
+          height={970}
+        />
+        <span className={styles.servicesContainerSubtitle}>
+          {t("servicesSection.find")}
+        </span>
+      </div>
+    </section>
+  );
+};
 
-export default ServicesSection
+export default ServicesSection;
