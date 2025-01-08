2fc835959ad76f89abbc0ebe25a0b3ab6f473e48
alejandro99so <alejosotodiaz@hotmail.com>
2025-01-08 14:58:50 -0500

    table hero section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "./globals.css";
+import HeroSection from "@/components/Sections/Hero/Hero";
 import ServicesSection from "@/components/Sections/Services/Services";
 import AboutSection from "@/components/Sections/About/About";
 import ContactSection from "@/components/Sections/Contact/Contact";
@@ -13,6 +14,7 @@ export default function Home() {
   return (
     <>
       {isLoading && <Loading text={textLoading} />}
+      <HeroSection />
       <ServicesSection />
       <AboutSection />
       <ContactSection
